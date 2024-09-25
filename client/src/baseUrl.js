ab2aab311674ffbf134b11b9ec4997309dec7c5b
ArushaK <arusha.kamate007@gmail.com>
2024-09-25 13:16:09 +0530

    Final code

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,5 +1,5 @@
-// const baseUrl = "https://mern-app-c5h5afavb3acc2dj.canadacentral-01.azurewebsites.net/";
+const baseUrl = "https://mern-app-c5h5afavb3acc2dj.canadacentral-01.azurewebsites.net/";
 
-const baseUrl = "http://localhost:3001"
+// const baseUrl = "http://localhost:3001"
 
 export default baseUrl;
