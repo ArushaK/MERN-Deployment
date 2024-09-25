626666c13e3096afc78827cf023d7576ec633e66
ArushaK <arusha.kamate007@gmail.com>
2024-09-25 14:30:24 +0530

    Final code

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = "mern-azure-adavd6g0agbfbtck.canadacentral-01.azurewebsites.net";
+const baseUrl = "https://mern-azure-adavd6g0agbfbtck.canadacentral-01.azurewebsites.net";
 
 // const baseUrl = "http://localhost:3001"
 
